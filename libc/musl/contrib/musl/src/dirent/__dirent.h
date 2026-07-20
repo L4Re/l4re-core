6ab038f94deee362e7f2b36d5a082245c1b9a6d0
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2026-07-20 00:00:00 +0000

    libc/musl: ensure proper alignment in struct __dirstream
    Our mutex type on L4Re is not just a single int. On 32 bit
    architectures, the libc_lock_t can accidentally create a bad alignment
    of the following "buf" field. Make sure it's always properly aligned.
    
    Change-Id: I26a9217a3fcd831cc3ff2d2471bfe20b8b3adaf7
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- libc/musl/contrib/musl/src/dirent/__dirent.h
+++ libc/musl/contrib/musl/src/dirent/__dirent.h
@@ -12,6 +12,8 @@ struct __dirstream
 	libc_lock_t lock;
 #endif
 	/* Any changes to this struct must preserve the property:
-	 * offsetof(struct __dirent, buf) % sizeof(off_t) == 0 */
-	char buf[2048];
+	 * offsetof(struct __dirent, buf) % sizeof(off_t) == 0
+	 * The L4 libc_lock_t replacement has a size/alignment that breaks this
+	 * on 32-bit architectures, so enforce off_t alignment for buf. */
+	char buf[2048] __attribute__((aligned(sizeof(off_t))));
 };
