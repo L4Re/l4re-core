719fda8bc75996df14be3cc86f085a91d1957c72
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2026-07-20 00:00:00 +0000

    libc/musl: only define 'hidden' for C/C++ code
    The MIPS assembler code, tramp-mips.S in particular, uses the ".hidden"
    directive. As the CPPFLAGS are applied to assembler sources too, it
    caused assembly to fail.
    
    Change-Id: I7025b800485612a748d8484b7017ad51dfd71e13
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- libc/musl/libpthread_vars.mk
+++ libc/musl/libpthread_vars.mk
@@ -1,6 +1,10 @@
 CFLAGS         += -include $(CONTRIB_DIR)/src/internal/libc.h
 CXXFLAGS       += -include $(CONTRIB_DIR)/src/internal/libc.h
-DEFINES        += -Dhidden='__attribute__((__visibility__("hidden")))'
+# Only define for C/C++, not for assembly: on MIPS the trampoline assembly uses
+# the '.hidden' directive, which would be mangled by a 'hidden' preprocessor
+# macro (DEFINES flows into CPPFLAGS and hence into the .S compile).
+CFLAGS         += -Dhidden='__attribute__((__visibility__("hidden")))'
+CXXFLAGS       += -Dhidden='__attribute__((__visibility__("hidden")))'
 CXXFLAGS       += -fno-exceptions
 PRIVATE_INCDIR += $(CONTRIB_DIR)/arch/$(LIBC_ARCH)
 
