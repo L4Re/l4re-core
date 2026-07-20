213f192b6357a321b890680f356d083e2d1a9345
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2026-07-20 00:00:00 +0000

    l4re_itas: remove musl specific amd64 signal part
    The musl signal structures are now compatible to the uclibc ones.
    Remove the musl specific adaptations.
    
    Change-Id: I3e5f51857ba8dd22a84447fe61012d31c93515dd
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- l4re_itas/server/src/ARCH-amd64/sig_arch.h
+++ l4re_itas/server/src/ARCH-amd64/sig_arch.h
@@ -7,6 +7,8 @@
  */
 #pragma once
 
+#include <assert.h>
+
 #include "../sig_arch_ifc.h"
 
 enum : l4_addr_t { Sig_stack_align = 16 };
@@ -81,30 +83,21 @@ static inline
 void setup_sighandler_frame(l4_exc_regs_t *u, ucontext_t *ucf,
                             siginfo_t const *si, struct sigaction const &sa)
 {
-#ifdef CONFIG_L4_LIBC_UCLIBC
   ucf->uc_mcontext.fpregs = &ucf->__fpregs_mem;
 
-  // Make sure there is enough space for fxsave64...
+  // Make sure there is enough space for fxsave64. Actually, we only need 464
+  // bytes since fxsave64 only writes at most 464 bytes. The memory  needs to
+  // be 16-byte aligned, because the fxsave64 instruction requires that, and
+  // otherwise raises an #GP.
   static_assert(sizeof(ucf->__fpregs_mem) >= 464);
-#else
-  // Make sure there is enough space for fxsave64...
-  // Actually we only need 464, since fxsave64 only writes a max of 464 bytes,
-  // but for the alignment we require +16. as the memory  needs to be 16-byte
-  // aligned, because the fxsave64 instruction requires that, and otherwise
-  // raises an #GP.
-  static_assert(sizeof(ucf->__fpregs_mem) >= 464 + 16);
-#endif
+  assert(reinterpret_cast<l4_addr_t>(&ucf->__fpregs_mem) % 16 == 0);
 
   u->ip = reinterpret_cast<l4_umword_t>(&sigenter);
   u->sp = reinterpret_cast<l4_addr_t>(ucf);
   u->rdi = si->si_signo;
   u->rsi = reinterpret_cast<l4_umword_t>(si);
   u->rdx = reinterpret_cast<l4_umword_t>(ucf);
-#ifdef CONFIG_L4_LIBC_UCLIBC
   u->rcx = reinterpret_cast<l4_umword_t>(&ucf->__fpregs_mem);
-#else
-  u->rcx = (reinterpret_cast<l4_umword_t>(&ucf->__fpregs_mem) + 15UL) & ~15UL;
-#endif
   u->r8 = (sa.sa_flags & SA_SIGINFO)
           ? reinterpret_cast<l4_umword_t>(sa.sa_sigaction)
           : reinterpret_cast<l4_umword_t>(sa.sa_handler);
